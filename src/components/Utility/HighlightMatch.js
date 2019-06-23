1474fc6a99a2ea4a71865a40523f5cf46805a04a
AnonyXCali <sauravezio@gmail.com>
2019-06-23 23:49:30 +0530

    removed console.log

--- src/components/Utility/HighlightMatch.js
+++ src/components/Utility/HighlightMatch.js
@@ -5,7 +5,6 @@ class HighlightMatch extends Component {
 
   getHighlightedText(text, higlight) {
     let parts = text.split(new RegExp(`(${higlight})`, 'gi'));
-    console.log(parts);
     return <span> { parts.map((part, i) =>
         <span key={i}
         style={part.toLowerCase() === higlight.toLowerCase() ? {  textDecoration:'underline', fontWeight: 'bold', color:'#4e8eee' } : {} }>
